74e42f0ccad541229f501f0b1b1223c224e61d00
parulchachan <parul.chachan@gmail.com>
2019-09-05 12:52:54 +0530

    add Post field

--- src/app/CompanyFolder/groupsFolder/domain-townhall/domain-townhall.component.ts
+++ src/app/CompanyFolder/groupsFolder/domain-townhall/domain-townhall.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-domain-townhall',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./domain-townhall.component.css']
 })
 export class DomainTownhallComponent implements OnInit {
-
+  messages:string[]=[];
+  
+  form= new FormGroup({
+    message: new FormControl('')
+  })
   constructor() { }
 
   ngOnInit() {
   }
+
+  Post(){
+    this.messages.push(this.form.get('message').value);
+    console.log(this.messages);
+    this.form.reset();
+  }  
 
 }
